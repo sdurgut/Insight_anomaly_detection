2449e931d82f512df6d258355bc37c6f00f6a6db
Suleyman Durgut <suleyman-durgut@uiowa.edu>
2017-07-02 19:03:41 -0500

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#python3 ./src/process_log.py ./log_input/batch_log.json ./log_input/stream_log.json ./log_output/flagged_purchases.json ./log_output/emails_tobe_send.txt
-python3 ./src/process_log.py ./sample_dataset/batch_log.json ./sample_dataset/stream_log.json ./log_output/flagged_purchases.json ./log_output/emails_tobe_send.txt
+python3 ./src/process_log.py ./log_input/batch_log.json ./log_input/stream_log.json ./log_output/flagged_purchases.json ./log_output/emails_tobe_send.txt
+#python3 ./src/process_log.py ./sample_dataset/batch_log.json ./sample_dataset/stream_log.json ./log_output/flagged_purchases.json ./log_output/emails_tobe_send.txt
 
